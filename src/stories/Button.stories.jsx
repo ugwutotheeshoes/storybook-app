783e66b0839d1c7f570785a14f4f1b01d951040b
Emmanuel Ugwu <>
2022-04-28 11:20:23 +0100

    new

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,5 +19,5 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   // primary: true,
-  label: 'Button',
+  label: 'Button 001',
 };
